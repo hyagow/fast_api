7bf3974027545ed39fc0e5098aa085da59b8eb22
Hyago Andrade <hyagosdj@gmail.com>
2024-07-15 17:16:22 -0300

    feat: insert models

---
+++ models/models.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from decimal import Decimal
+
+class ContaPagarReceberResponse(BaseModel):
+  id: int
+  descricao: str
+  valor: float | Decimal=Field(..., description="Valor da conta", example='100.40')
+  tipo: str
+
+
+# class ContaPagarReceberResponse(Contas): ...
