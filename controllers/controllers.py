924a0fddf92a5e684dc2f6f49edc90d0e221c626
Hyago Andrade <hyagosdj@gmail.com>
2024-07-20 00:06:55 -0300

    feat: correction post_conta to criar_conta

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter  #type: ignore
-from models.models import ContaPagarReceberResponse
+from models.models import ContaPagarReceberResponse, ContaPagarReceberRequest
 from typing import List
-from decimal import Decimal
+# from decimal import Decimal
 
 
 router = APIRouter(prefix="/contas")
 
-db_conta = [
-  {"id": 1, "descricao": "Aluguel", "valor": 900, "tipo": "PAGAR"},
-  {"id": 2, "descricao": "Curso", "valor": 800.20, "tipo": "PAGAR"},
-]
+# db_conta = [
+#   {"id": 1, "descricao": "Aluguel", "valor": 900, "tipo": "PAGAR"},
+#   {"id": 2, "descricao": "Curso", "valor": 800.20, "tipo": "PAGAR"},
+# ]
 
 @router.get("/", response_model=List[ContaPagarReceberResponse])
 def listar_contas():
@@ -17,7 +17,7 @@ def listar_contas():
     ContaPagarReceberResponse(
       id=1,
       descricao="Aluguel",
-      valor=Decimal(900),
+      valor=1000,
       tipo="PAGAR"
     ),
     ContaPagarReceberResponse(
@@ -28,6 +28,17 @@ def listar_contas():
     )
   ]
 
+@router.post("/", response_model=ContaPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContaPagarReceberRequest):
+  return ContaPagarReceberResponse(
+    id=3,
+    descricao=conta.descricao,
+    valor=conta.valor,
+    tipo=conta.tipo
+  )
+
+
+
 # @router.get("/", response_model=List[ContaPagarReceberResponse])
 # def listar_uma_conta(conta_id: int):
 #   for key, value in 
@@ -36,19 +47,8 @@ def listar_contas():
 #   else:
 #     raise ValueError("Item not found")
 
-@router.post("/", response_model=ContaPagarReceberResponse)
-def post_conta(id: int, descricao: str, valor: Decimal, tipo: str):
-  if not id or not descricao or not valor or not tipo:
-    raise ValueError("Produto não sem nome ou preço")
-  return f'Item inserted successfully: {\
-    "id": id, \
-    "descricao": descricao, \
-    "valor": valor, \
-    "tipo": tipo
-    }'
-
-@router.put("/", response_model=ContaPagarReceberResponse)
-def update_conta(conta_id: int):
-  if conta_id >= len(db_conta):
-    raise ValueError("Conta não encontrada")
-  return db_conta[conta_id]
+# @router.put("/", response_model=ContaPagarReceberResponse)
+# def update_conta(conta_id: int):
+#   if conta_id >= len(db_conta):
+#     raise ValueError("Conta não encontrada")
+#   return db_conta[conta_id]
