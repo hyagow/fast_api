ffe22b13423b2df56fe499255da5c6313d52966f
Hyago Andrade <hyagosdj@gmail.com>
2024-07-19 23:17:07 -0300

    feat: insert update_conta in controllers

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -6,6 +6,11 @@ from decimal import Decimal
 
 router = APIRouter(prefix="/contas")
 
+db_conta = [
+  {"id": 1, "descricao": "Aluguel", "valor": 900, "tipo": "PAGAR"},
+  {"id": 2, "descricao": "Curso", "valor": 800.20, "tipo": "PAGAR"},
+]
+
 @router.get("/", response_model=List[ContaPagarReceberResponse])
 def listar_contas():
   return [
@@ -32,7 +37,18 @@ def listar_contas():
 #     raise ValueError("Item not found")
 
 @router.post("/", response_model=ContaPagarReceberResponse)
-def post_item(id: int, descricao: str, valor: Decimal, tipo: str):
+def post_conta(id: int, descricao: str, valor: Decimal, tipo: str):
   if not id or not descricao or not valor or not tipo:
     raise ValueError("Produto não sem nome ou preço")
-  return f'Item inserted successfully: {"id": id, "descricao": descricao, "valor": valor, "tipo": tipo}'
+  return f'Item inserted successfully: {\
+    "id": id, \
+    "descricao": descricao, \
+    "valor": valor, \
+    "tipo": tipo
+    }'
+
+@router.put("/", response_model=ContaPagarReceberResponse)
+def update_conta(conta_id: int):
+  if conta_id >= len(db_conta):
+    raise ValueError("Conta não encontrada")
+  return db_conta[conta_id]
