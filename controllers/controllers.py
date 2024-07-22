eee21c411c5551c162b8eb964f2299a97a539aa3
Hyago Andrade <hyagosdj@gmail.com>
2024-07-22 00:07:13 -0300

    feat: modified def listar_contas, def listar_uma_conta, def criar_conta and inserted db_contas

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,54 +1,59 @@
-from fastapi import APIRouter  #type: ignore
+from fastapi import APIRouter, HTTPException, status
 from models.models import ContaPagarReceberResponse, ContaPagarReceberRequest
 from typing import List
-# from decimal import Decimal
 
 
-router = APIRouter(prefix="/contas")
+router = APIRouter(prefix="/contas", tags=["Contas"])
 
-# db_conta = [
-#   {"id": 1, "descricao": "Aluguel", "valor": 900, "tipo": "PAGAR"},
-#   {"id": 2, "descricao": "Curso", "valor": 800.20, "tipo": "PAGAR"},
-# ]
+db_contas = [
+  {"id": 1, "descricao": "Aluguel", "valor": 900, "tipo": "PAGAR"},
+  {"id": 2, "descricao": "Curso", "valor": 800.20, "tipo": "PAGAR"},
+]
 
 @router.get("/", response_model=List[ContaPagarReceberResponse])
-def listar_contas():
-  return [
-    ContaPagarReceberResponse(
-      id=1,
-      descricao="Aluguel",
-      valor=1000,
-      tipo="PAGAR"
-    ),
-    ContaPagarReceberResponse(
-      id=2,
-      descricao="Curso",
-      valor=800.120,
-      tipo="PAGAR"
-    )
-  ]
+def listar_contas() -> list:
+  return db_contas
 
+@router.get("/conta/{id}", response_model=ContaPagarReceberResponse, status_code=200)
+def listar_uma_conta(conta_id: int) -> dict:
+  for conta in db_contas:
+    if conta["id"] == conta_id:
+      return conta
+  else:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=f'Conta do id: {conta_id}, não foi localizada.')
+  
 @router.post("/", response_model=ContaPagarReceberResponse, status_code=201)
-def criar_conta(conta: ContaPagarReceberRequest):
-  return ContaPagarReceberResponse(
-    id=3,
-    descricao=conta.descricao,
-    valor=conta.valor,
-    tipo=conta.tipo
-  )
+def criar_conta(conta: ContaPagarReceberRequest) -> dict:
+  if conta:
+    new_conta = {
+      "id":len(db_contas)+1, 
+      "descricao":conta.descricao,
+      "valor":conta.valor,
+      "tipo":conta.tipo    
+      }
+    db_contas.append(new_conta)
+  return new_conta
 
 
+@router.put("/conta/{id}", response_model=ContaPagarReceberRequest, status_code=200)
+def atualizar_conta(conta_id: int, conta_update: ContaPagarReceberRequest) -> dict:
+  for conta in db_contas:
+    if conta["id"] == conta_id:
+      conta["descricao"] = conta_update.descricao
+      conta["valor"] = conta_update.valor
+      conta["tipo"] = conta_update.tipo
+      return conta
+  else:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=f'Conta do id: {conta_id}, não foi localizada.')
 
-# @router.get("/", response_model=List[ContaPagarReceberResponse])
-# def listar_uma_conta(conta_id: int):
-#   for key, value in 
-#   if conta_id == id.ContaPagarReceberResponse:
-#     return [ContaPagarReceberResponse]
-#   else:
-#     raise ValueError("Item not found")
-
-# @router.put("/", response_model=ContaPagarReceberResponse)
-# def update_conta(conta_id: int):
-#   if conta_id >= len(db_conta):
-#     raise ValueError("Conta não encontrada")
-#   return db_conta[conta_id]
+@router.delete("/conta/{id}", status_code=204)
+def deletar_conta(conta_id: int):
+  for conta in db_contas:
+    if conta["id"] == conta_id:
+      db_contas.remove(conta)
+      return print(f'Conta do id: {conta_id}, foi deletada.')
+  else:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=f'Conta do id: {conta_id}, não foi localizada.')
