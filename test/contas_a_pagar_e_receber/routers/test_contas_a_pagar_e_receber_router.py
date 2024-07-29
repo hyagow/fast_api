cfcb129e9dc9833508fc8c6877c99338375a672e
Hyago Andrade <hyagosdj@gmail.com>
2024-07-29 18:24:12 -0300

    feat: insert test_deve_listar_contas_a_pagar_e_receber()

--- test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
+++ test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
@@ -4,3 +4,11 @@ from main import app
 
 client = TestClient(app)
 
+def test_deve_listar_contas_a_pagar_e_receber():
+  response = client.get('/contas_a_pagar_e_receber')
+
+  assert response.status_code == 200
+  assert response.json() == [
+    {'id': 1, 'descricao': 'Aluguel', 'valor': '900', 'tipo': 'PAGAR'}, 
+    {'id': 2, 'descricao': 'Curso', 'valor': 800.2, 'tipo': 'PAGAR'},
+    ]
