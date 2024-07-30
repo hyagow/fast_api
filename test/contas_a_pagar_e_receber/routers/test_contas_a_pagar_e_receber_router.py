61f4251eac842c39f16565f088df8cfb65dee088
Hyago Andrade <hyagosdj@gmail.com>
2024-07-30 15:46:18 -0300

    feat: insert test_deve_criar_conta_a_pagar_e_receber()

--- test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
+++ test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
@@ -12,3 +12,16 @@ def test_deve_listar_contas_a_pagar_e_receber():
     {'id': 1, 'descricao': 'Aluguel', 'valor': '900', 'tipo': 'PAGAR'}, 
     {'id': 2, 'descricao': 'Curso', 'valor': 800.2, 'tipo': 'PAGAR'},
     ]
+  
+def test_deve_criar_conta_a_pagar_e_receber():
+  nova_conta = {
+    "descricao": "Curso de Python",
+    "valor": "333",
+    "tipo": "PAGAR"
+  }
+  nova_conta_copy = nova_conta.copy()
+  nova_conta_copy["id"] = 3
+
+  response = client.post("/contas_a_pagar_e_receber", json=nova_conta)
+  assert response.status_code == 201
+  assert response.json() == nova_conta_copy
