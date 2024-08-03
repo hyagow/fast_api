adbd3d5b95067e89bce626255cc97ac8cbb4c611
Hyago Andrade <hyagosdj@gmail.com>
2024-08-03 16:04:03 -0300

    feat: insert commentary to models

--- models/models.py
+++ models/models.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, Field
 from decimal import Decimal
 
+# Model to Response
 class ContaPagarReceberResponse(BaseModel):
   id: int
   descricao: str
   valor: float | Decimal=Field(..., description="Valor da conta")
   tipo: str
 
-
+# Model to Request
 class ContaPagarReceberRequest(BaseModel):
   descricao: str
   valor: float | Decimal=Field(..., description="Valor da conta")
