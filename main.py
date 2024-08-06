d57b02e178a9f43432f52f02d14b63136a7e5b22
Hyago Andrade <hyagosdj@gmail.com>
2024-08-05 22:41:49 -0300

    feat: insert commentary to main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI  #type: ignore
 from controllers import controllers
 
 
-
+# Instância do FastAPI
 app = FastAPI(title="ContasAPI", description="API para gerenciamento de contas")
 
-
+# Incrementando rotas
 app.include_router(controllers.router)
 
-
+# Inicialização do programa
 if __name__ == '__main__':
   uvicorn.run(app, host='127.0.0.1', port=8080)
