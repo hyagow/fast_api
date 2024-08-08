4d390ab5373d4db7e125624d1b6851b5ff7942e0
Hyago Andrade <hyagosdj@gmail.com>
2024-08-08 14:09:00 -0300

    feat: insert class Contas(Base) to models in database

--- models/models.py
+++ models/models.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
 from decimal import Decimal
+from sqlalchemy import Column, Integer, String, Float
+from db.database import Base
+
 
 # Model to Response
 class ContaPagarReceberResponse(BaseModel):
@@ -13,3 +16,10 @@ class ContaPagarReceberRequest(BaseModel):
   descricao: str
   valor: float | Decimal=Field(..., description="Valor da conta")
   tipo: str
+
+class Contas(Base):
+    __tablename__ = 'contas'
+    id = Column(Integer, primary_key=True, index=True)
+    descricao = Column(String, index=True)
+    valor = Column(Float)
+    tipo = Column(String, index=True)
