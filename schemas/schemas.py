2ad5d5133b05ab85eb1b7b1caad3a9ba490fceca
Hyago Andrade <hyagosdj@gmail.com>
2024-08-10 01:36:16 -0300

    feat: insert commentary to class user , usercreate, userbase

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel
 
+# Schema to characterist of user
 class UserBase(BaseModel):
     name: str
     age: int
 
+# Schema to creation of user
 class UserCreate(UserBase):
     pass
 
+# Schema to default user
 class User(UserBase):
     id: int
 
